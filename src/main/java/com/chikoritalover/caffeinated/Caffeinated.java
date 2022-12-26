6f8858f328c9fa7d117245dcd35933ca94291df3
Chikorita Lover <cjgardels@gmail.com>
2022-12-26 13:40:49 -0600

    Initialize project

--- src/main/java/com/chikoritalover/caffeinated/Caffeinated.java
+++ src/main/java/com/chikoritalover/caffeinated/Caffeinated.java
@@ -1,21 +1,18 @@
-package net.fabricmc.example;
+package com.chikoritalover.caffeinated;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class Caffeinated implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MODID = "caffeinated";
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
