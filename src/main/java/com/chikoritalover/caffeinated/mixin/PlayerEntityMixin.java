f19e6d2ca982cd22ea16d379fa4da181654dc43d
Chikorita Lover <cjgardels@gmail.com>
2023-01-04 17:32:45 -0600

    Fix Caffeine random generator

--- src/main/java/com/chikoritalover/caffeinated/mixin/PlayerEntityMixin.java
+++ src/main/java/com/chikoritalover/caffeinated/mixin/PlayerEntityMixin.java
@@ -7,8 +7,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
     @Inject(method = "addExhaustion", at = @At(value = "HEAD"), cancellable = true)
@@ -16,7 +14,7 @@ public class PlayerEntityMixin {
         PlayerEntity player = PlayerEntity.class.cast(this);
         if (player.hasStatusEffect(ModStatusEffects.CAFFEINE)) {
             int amplifier = player.getStatusEffect(ModStatusEffects.CAFFEINE).getAmplifier();
-            if (player.world.getRandom().nextBetween(0, amplifier + 2) > 1) {
+            if (player.getRandom().nextBetween(0, amplifier + 2) > 1) {
                 ci.cancel();
             }
         }
