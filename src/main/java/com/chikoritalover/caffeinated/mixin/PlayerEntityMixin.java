9751d2affd787f5dd86fe7964457d899888a3710
Chikorita Lover <cjgardels@gmail.com>
2023-01-03 22:49:09 -0600

    Update Caffeine effect
    Caffeine now has a 1/3 chance of preventing exhaustion, as opposed to guaranteed reduced exhaustion

--- src/main/java/com/chikoritalover/caffeinated/mixin/PlayerEntityMixin.java
+++ src/main/java/com/chikoritalover/caffeinated/mixin/PlayerEntityMixin.java
@@ -7,14 +7,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Objects;
+
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
     @Inject(method = "addExhaustion", at = @At(value = "HEAD"), cancellable = true)
     private void addExhaustion(float exhaustion, CallbackInfo ci) {
         PlayerEntity player = PlayerEntity.class.cast(this);
-        if (player.hasStatusEffect(ModStatusEffects.CAFFEINE) && !player.world.isClient() && !player.getAbilities().invulnerable) {
-            player.getHungerManager().addExhaustion(exhaustion / 2);
-            ci.cancel();
+        if (player.hasStatusEffect(ModStatusEffects.CAFFEINE)) {
+            int amplifier = player.getStatusEffect(ModStatusEffects.CAFFEINE).getAmplifier();
+            if (player.world.getRandom().nextBetween(0, amplifier + 2) > 1) {
+                ci.cancel();
+            }
         }
     }
 }
