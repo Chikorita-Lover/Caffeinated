aac2b2f48b8f836cdc9186e8d20a6ce1e69f89f5
ExDrill <72329798+ExDrill@users.noreply.github.com>
2023-01-12 18:53:55 -0500

    Farmer's Delight Item Group

---
+++ src/main/java/com/chikoritalover/caffeinated/integration/farmersdelight/FDItemGroup.java
@@ -0,0 +1,11 @@
+package com.chikoritalover.caffeinated.integration.farmersdelight;
+
+import com.nhoryzon.mc.farmersdelight.FarmersDelightMod;
+import net.minecraft.item.ItemGroup;
+
+public class FDItemGroup {
+
+    public static ItemGroup getFDItemGroup() {
+        return FarmersDelightMod.ITEM_GROUP;
+    }
+}
--- src/main/java/com/chikoritalover/caffeinated/registry/ModItems.java
+++ src/main/java/com/chikoritalover/caffeinated/registry/ModItems.java
@@ -1,14 +1,17 @@
 package com.chikoritalover.caffeinated.registry;
 
 import com.chikoritalover.caffeinated.Caffeinated;
+import com.chikoritalover.caffeinated.integration.farmersdelight.FDItemGroup;
 import com.chikoritalover.caffeinated.item.CoffeeBottleItem;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.AliasedBlockItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.Nullable;
 
 public class ModItems {
     public static final Item COFFEE_BEAN_BLOCK = new BlockItem(ModBlocks.COFFEE_BEAN_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
@@ -37,5 +40,13 @@ public class ModItems {
         CompostingChanceRegistry.INSTANCE.add(COFFEE_BEANS, 0.5F);
         CompostingChanceRegistry.INSTANCE.add(GROUND_COFFEE, 0.5F);
         CompostingChanceRegistry.INSTANCE.add(COFFEE_BERRIES, 0.65F);
+    }
+
+    @Nullable
+    public static ItemGroup getCrateItemGroup() {
+        if (!FabricLoader.getInstance().isModLoaded("farmersdelight")) {
+            return null;
+        }
+        return FDItemGroup.getFDItemGroup();
     }
 }
