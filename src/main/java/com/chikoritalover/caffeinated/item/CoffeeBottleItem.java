5c28fff3c3b7f0945157ffca26867a09b861b574
Chikorita Lover <cjgardels@gmail.com>
2023-12-29 16:06:58 -0600

    Add coffee bottle tooltip

--- src/main/java/com/chikoritalover/caffeinated/item/CoffeeBottleItem.java
+++ src/main/java/com/chikoritalover/caffeinated/item/CoffeeBottleItem.java
@@ -14,6 +14,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
@@ -75,6 +76,12 @@ public class CoffeeBottleItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         return ItemUsage.consumeHeldItem(world, user, hand);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+        tooltip.add(Text.translatable(this.getTranslationKey() + ".desc").formatted(Formatting.GRAY));
     }
 }
 
