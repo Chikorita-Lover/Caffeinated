b139aad55d27302a30b07be5de93a4f2107fd92c
Chikorita Lover <cjgardels@gmail.com>
2025-09-10 23:59:54 -0500

    Update coffee-related trades
    Farmer villagers no longer buy coffee berries
    The price of coffee berries from wandering traders has been increased to 3 emeralds
    Wandering traders may now buy coffee bottles for 3 emeralds (Villager Trade Rebalance only)

--- src/main/java/net/chikorita_lover/caffeinated/registry/CaffeinatedTradeOffers.java
+++ src/main/java/net/chikorita_lover/caffeinated/registry/CaffeinatedTradeOffers.java
@@ -2,15 +2,17 @@ package net.chikorita_lover.caffeinated.registry;
 
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.minecraft.village.TradeOffers;
-import net.minecraft.village.VillagerProfession;
 
 public class CaffeinatedTradeOffers {
     public static void register() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 3, factories -> {
-            factories.add(new TradeOffers.BuyItemFactory(CaffeinatedItems.COFFEE_BERRIES, 14, 12, 20));
-        });
         TradeOfferHelper.registerWanderingTraderOffers(1, factories -> {
-            factories.add(new TradeOffers.SellItemFactory(CaffeinatedItems.COFFEE_BERRIES, 1, 1, 12, 1));
+            factories.add(new TradeOffers.SellItemFactory(CaffeinatedItems.COFFEE_BERRIES, 3, 1, 8, 1));
+        });
+        TradeOfferHelper.registerRebalancedWanderingTraderOffers(builder -> {
+            builder.addOffersToPool(TradeOfferHelper.WanderingTraderOffersBuilder.BUY_ITEMS_POOL, new TradeOffers.BuyItemFactory(CaffeinatedItems.COFFEE_BOTTLE, 1, 2, 1, 3));
+        });
+        TradeOfferHelper.registerRebalancedWanderingTraderOffers(builder -> {
+            builder.addOffersToPool(TradeOfferHelper.WanderingTraderOffersBuilder.SELL_COMMON_ITEMS_POOL, new TradeOffers.SellItemFactory(CaffeinatedItems.COFFEE_BERRIES, 3, 1, 8, 1));
         });
     }
 }
